e47e86755dafd763bf0b48ebb59fde631c640dc2
PedroHenriqueed <pedrohenrique12davi@gmail.com>
2025-05-19 19:50:03 -0300

    Removendo uma parte  do codigo do script.js

--- script.js
+++ script.js
@@ -16,18 +16,3 @@ window.onclick = (event) => {
   }
 };
 
-const dropdown = document.querySelector(".dropdown");
-const dropdownContent = dropdown.querySelector(".dropdown-content");
-
-let timeout;
-
-dropdown.addEventListener('mouseenter', () =>{
-  clearTimeout(timeout);
-  dropdownContent.style.display = "block";
-});
-
-dropdown.addEventListener('mouseleave', () =>{
-  timeout = setTimeout(() =>{
-    dropdownContent.style.display = "none";
-  }, 300);
-});
